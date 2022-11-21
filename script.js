9797ace707d08cfcad60efb0c3019d7fc1a2cf25
TerranceReimar <terrancereimar@gmail.com>
2022-11-21 13:09:45 +0200

    Working Etch a Sketch project to be posted on github.

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-const container = document.querySelector(".container");
-const column = document.querySelector(".column");
-const row = document.querySelector(".row");
+const screen = document.querySelector(".screen");
+smallGrid();
 
-for (let i = 0; i < 5; i++) {
-  const div = document.createElement("div");
-  div.classList.add("smallGrid");
-  column.appendChild(div);
+function smallGrid() {
+  for (let i = 0; i < 16 * 16; i++) {
+    let div = document.createElement("div");
+    div.classList.add("grid", "smallGrid");
+    screen.append(div);
+    div.addEventListener("mouseover", function () {
+      div.classList.add("gridActive");
+    });
+  }
 }
+
+function bigGrid() {
+  for (let i = 0; i < 32 * 32; i++) {
+    let div = document.createElement("div");
+    div.classList.add("grid", "bigGrid");
+    screen.append(div);
+    div.addEventListener("mouseover", function () {
+      div.classList.add("gridActive");
+    });
+  }
+}
+
+const bigButton = document.querySelector(".bigButton");
+bigButton.addEventListener("click", function () {
+  screen.textContent = "";
+  bigGrid();
+});
+
+const smallButton = document.querySelector(".smallButton");
+smallButton.addEventListener("click", function () {
+  screen.textContent = "";
+  smallGrid();
+});
